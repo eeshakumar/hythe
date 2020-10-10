0518731362d18b2c24802d3c28c982d6a21273eb
Eesha Kumar <mee.59912011@gmail.com>
2020-10-10 22:04:02 +0200

    Timer precision to 8 decimal places

--- libs/timer/timer.py
+++ libs/timer/timer.py
@@ -7,6 +7,6 @@ def timer(func):
     def wrapper(*args, **kwargs):
         start = time()
         res = func(*args, **kwargs)
-        print(f"Function {func.__name__} took {time() - start:0.4f}s")
+        print(f"Function {func.__name__} took {time() - start:0.8f}s")
         return res
     return wrapper
