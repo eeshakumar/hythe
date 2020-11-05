a9076528ad1ac6af52a6f3b38c2f897da6a90f87
Eesha Kumar <mee.59912011@gmail.com>
2020-11-05 23:50:19 +0100

    Thresholding and discretization for beliefs

--- libs/observer/belief_observer.py
+++ libs/observer/belief_observer.py
@@ -5,7 +5,6 @@
 import numpy as np
 from gym import spaces
 from collections import OrderedDict
-# from bark_mcts.models.behavior.belief_calculator.belief_calculator import BeliefCalculator
 from bark_ml.observers.nearest_state_observer import NearestAgentsObserver
 from bark.core.world import World, MakeTestWorldHighway, ObservedWorld
 from bark.core.models.behavior import *
@@ -22,6 +21,12 @@ class BeliefObserver(NearestAgentsObserver):
         """
         super(BeliefObserver, self).__init__(params)
         self.params = params
+        self.is_enabled_threshold = params["ML"]["BeliefObserver"]["EnableThreshold", "If Threshold to cutoff beliefs must be enabled", True]
+        self.threshold = self.params["ML"]["BeliefObserver"]["Threshold", "Cutoff threshold for beliefs", 0.05]
+        self.is_discretize = params["ML"]["BeliefObserver"]["Discretize", "If value of belief must be discretized", False]
+        self.num_buckets = params["ML"]["BeliefObserver"]["NumBuckets", "Number of discrete values to map beliefs", 8]
+        self.is_ciel = params["ML"]["BeliefObserver"]["CielValues", "If to Ceil or Floor value", True]
+        self.buckets = np.linspace(0., 1., self.num_buckets)
         self.hypothesis_set = hypothesis_set
         self.splits = splits
         self.belief_calculator = BeliefCalculator(params, hypothesis_set)
@@ -52,6 +57,10 @@ class BeliefObserver(NearestAgentsObserver):
         beliefs = self.belief_calculator.GetBeliefs()
         self.beliefs = self.order_beliefs(beliefs, agent_ids_by_nearest)
         len_beliefs, beliefs_arr = self.beliefs_array()
+        if self.is_enabled_threshold:
+          beliefs_arr = self.threshold_beliefs(beliefs_arr)
+        if self.is_discretize:
+          beliefs_arr = self.discretize_beliefs(beliefs_arr)
         assert len_beliefs == self.len_beliefs
         beliefs_concatenated_state = np.concatenate([concatenated_state, beliefs_arr])
         return beliefs_concatenated_state
@@ -84,6 +93,15 @@ class BeliefObserver(NearestAgentsObserver):
             high=np.zeros(self.len_beliefs + self._len_ego_state + \
         self._max_num_vehicles*self._len_relative_agent_state)
         )
+
+    def threshold_beliefs(self, beliefs_arr):
+        beliefs_arr[beliefs_arr <= self.threshold] = 0.
+        return beliefs_arr
+
+    def discretize_beliefs(self, beliefs_arr):
+        bin_idxs = np.digitize(beliefs_arr, self.buckets, right=self.is_ciel)
+        assert bin_idxs.shape[0] == beliefs_arr.shape[0]
+        return self.buckets[bin_idxs]
 
     @property
     def max_num_agents(self):
