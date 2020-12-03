bf28968dd352d18f3ec6b9d939b000ef473f9579
Eesha Kumar <mee.59912011@gmail.com>
2020-12-03 13:16:20 +0100

    Script to train for learning from demonstrations

--- configuration/src/hy_iqnfd_runner.py
+++ configuration/src/hy_iqnfd_runner.py
@@ -16,7 +16,8 @@ from hythe.libs.environments.gym import HyDiscreteHighway, GymSingleAgentRuntime
 from bark_ml.evaluators.goal_reached_guiding import GoalReachedGuiding
 from bark_ml.evaluators.goal_reached import GoalReached
 from bark_ml.library_wrappers.lib_fqf_iqn_qrdqn.agent import IQNAgent
-from bark_ml.library_wrappers.lib_fqf_iqn_qrdqn.agent.demonstrations import DemonstrationCollector
+from bark_ml.library_wrappers.lib_fqf_iqn_qrdqn.agent.demonstrations import (
+  DemonstrationCollector, DemonstrationGenerator)
 
 from bark_ml.observers.nearest_state_observer import NearestAgentsObserver
 from bark_ml.behaviors.discrete_behavior import BehaviorDiscreteMacroActionsML
@@ -30,15 +31,49 @@ add_config_reader_module("bark_mcts.runtime.scenario.behavior_space_sampling")
 
 from libs.evaluation.training_benchmark_database import TrainingBenchmarkDatabase
 
+from bark_ml.library_wrappers.lib_fqf_iqn_qrdqn.\
+            tests.test_demo_behavior import TestDemoBehavior          
+
 
 is_local = True
 
 if is_local:
   num_episodes = 10
   num_scenarios = 5
+  num_demo_episodes = 4
 else:
   num_episodes = 50000
   num_scenarios = 1000
+  num_demo_episodes = 1000
+
+
+# TODO: Remove this evaluator
+from bark_ml.evaluators.evaluator import StateEvaluator
+class TestEvaluator(StateEvaluator):
+  reach_goal = True
+  def __init__(self,
+              params=ParameterServer()):
+    StateEvaluator.__init__(self, params)
+    self.step = 0
+
+  def _evaluate(self, observed_world, eval_results, action):
+    """Returns information about the current world state
+    """
+    done = False
+    reward = 0.0
+    info = {"goal_r1" : False}
+    if self.step > 2:
+      done = True
+      if self.reach_goal:
+        reward = 0.1
+        info = {"goal_r1" : True}
+    self.step += 1
+    return reward, done, info
+
+  def Reset(self, world):
+    self._step = 0
+    #every second scenario goal is not reached
+    TestEvaluator.reach_goal = not TestEvaluator.reach_goal
 
 
 def configure_args(parser=None):
@@ -52,10 +87,27 @@ def configure_args(parser=None):
 def configure_agent(params, env):
     agent_save_dir = os.path.join(params["Experiment"]["dir"], "agent")
     training_benchmark = None #TrainingBenchmarkDatabase()
-    demo_collector = DemonstrationCollector()
-    agent = IQNAgent(env=env, params=params, agent_save_dir=agent_save_dir, training_benchmark=training_benchmark,
-                     demo_collector=demo_collector)
+    agent = IQNAgent(env=env, params=params, agent_save_dir=agent_save_dir,
+                     training_benchmark=training_benchmark, is_learn_from_demonstrations=True)
     return agent
+
+
+def generate_demonstrations(params, env, eval_criteria, demo_behavior=None):
+    # TODO: Real objects need not be copied this way. Demo env can be created new.
+    real_behavior = env._ml_behavior
+    real_evaluator = env._evaluator
+    env._evaluator = TestEvaluator()
+    demo_behavior = TestDemoBehavior(params)
+    demo_collector = DemonstrationCollector()
+    save_dir = os.path.join(params["Experiment"]["dir"], "demonstrations")
+    demo_generator = DemonstrationGenerator(env, params, demo_behavior, demo_collector, save_dir)
+    if not os.path.exists(save_dir):
+      os.makedirs(save_dir)
+    demo_generator.generate_demonstrations(num_demo_episodes, eval_criteria, save_dir)
+    demo_generator.dump_demonstrations(save_dir)
+    env._evaluator = real_evaluator
+    env._ml_behavior = real_behavior
+    return demo_generator.demonstrations
 
 
 def configure_params(params, seed=None):
@@ -80,9 +132,13 @@ def run(params, env, exp_exists=False):
         print("Loading from last best checkpoint.")
         agent.load_models(agent_checkpoint_last)
       else:
-        print("No checkpoint written.") 
+        print("No checkpoint written.")
+
+    #TODO: Revisit this criteria
+    eval_criteria = {"goal_r1" : lambda x : x}
+    demonstrations = generate_demonstrations(params, env, eval_criteria)
     exp = Experiment(params=params, agent=agent, dump_scenario_interval=25000)
-    exp.run(demonstrator=True)
+    exp.run(demonstrator=True, demonstrations=demonstrations)
 
 
 def check_if_exp_exists(params):
