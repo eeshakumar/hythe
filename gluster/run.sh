f320cfe610ba7e64959e53aa6a948513b007eb0b
Eesha Kumar <mee.59912011@gmail.com>
2021-01-19 12:35:53 +0100

    Rectified GPU usage issue

--- gluster/run.sh
+++ gluster/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --qos lowprio_students
 #SBATCH -c 4
-##SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1
 #SBATCH --mem=10GB
 
 srun singularity exec --nv /mnt/glusterdata/home/ekumar/images/hythe_latest_2.sif python3 -u ./$1 $2
